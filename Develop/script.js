09807f36343a7f0afda0fd003c065122fe9206d7
Mr. Hood <adamhood15@gmail.com>
2023-03-21 07:26:22 -0500

    Added ready function

--- Develop/script.js
+++ Develop/script.js
@@ -3,9 +3,14 @@
 // in the html.
 
 var saveBtn = $('.saveBtn');
+var currentDay = $('#currentDay');
 
+$(document).ready(function () {
 
-$(function () {
+  var today = dayjs().format('dddd MMMM DD');
+  currentDay.text(today);
+
+}) ;
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -27,4 +32,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
