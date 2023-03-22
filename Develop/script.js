c06621024793a2f6808c490e5ddcab072d984e05
Adam Hood <adamhood15@gmail.com>
2023-03-22 13:01:20 -0500

    Refactored event listener function to be much smaller

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var get9amEvent = localStorage.getItem('9amEvent');
+var get9amEven = localStorage.getItem('9amEvent');
 var saveBtn = $('.saveBtn');
 var currentDay = $('#currentDay');
-var textInput9am = $('#9am');
-var textInput10am = $('#10am');
-var textInput11am = $('#11am');
-var textInput12am = $('#12am');
-var textInput1pm = $('#1pm');
-var textInput2pm = $('#2pm');
-var textInput3pm = $('#3pm');
-var textInput4pm = $('#4pm');
-var textInput5pm = $('#5pm');
-var saveBtn9am = $('#9amBtn')
-var saveBtn10am = $('#10amBtn')
-var saveBtn11am = $('#11amBtn')
-var saveBtn12am = $('#12amBtn')
-var saveBtn1pm = $('#1pmBtn')
-var saveBtn2pm = $('#2pmBtn')
-var saveBtn3pm = $('#3pmBtn')
-var saveBtn4pm = $('#4pmBtn')
-var saveBtn5pm = $('#5pmBtn')
+var event9am = $('#9am')
+var event10am = $('#10am') 
+var event11am = $('#11am')
+var event12am = $('#12am')
+var event1pm = $('#1pm')
+var event2pm = $('#2pm')
+var event3pm = $('#3pm')
+var event4pm = $('#4pm')
+var event5pm = $('#5pm')
+
+
+
+
 
 var calendarEvents = [];
 
@@ -39,80 +34,27 @@ $(document).ready(function () {
   // useful when saving the description in local storage?
   //
 
-textInput9am.text(get9amEvent);
-textInput10am.text(localStorage.getItem('10amEvent'));
-textInput11am.text(localStorage.getItem('11amEvent'));
-textInput12am.text(localStorage.getItem('12amEvent'));
-textInput1pm.text(localStorage.getItem('1pmEvent'));
-textInput2pm.text(localStorage.getItem('2pmEvent'));
-textInput3pm.text(localStorage.getItem('3pmEvent'));
-textInput4pm.text(localStorage.getItem('4pmEvent'));
-textInput5pm.text(localStorage.getItem('5pmEvent'));
+event9am.val(localStorage.getItem('hour-9'));
+event10am.val(localStorage.getItem('hour-10'));
+event11am.val(localStorage.getItem('hour-11'));
+event12am.val(localStorage.getItem('hour-12'));
+event1pm.val(localStorage.getItem('hour-1'));
+event2pm.val(localStorage.getItem('hour-2'));
+event3pm.val(localStorage.getItem('hour-3'));
+event4pm.val(localStorage.getItem('hour-4'));
+event5pm.val(localStorage.getItem('hour-5'));
 
-saveBtn9am.on('click', function (event) {
-  console.log($(this));
 
-    let userInput = textInput9am.val();
-    var storeEvent = localStorage.setItem('9amEvent', userInput);
+saveBtn.on('click', function (event) {
+  var parentID = event.target.parentElement.parentElement.id;
+  var siblingID = event.target.parentElement.previousElementSibling;
+  var userInput = siblingID.value;
 
-})
+  localStorage.setItem(parentID, userInput)
 
-saveBtn10am.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput10am.val();
-    var storeEvent = localStorage.setItem('10amEvent', userInput);
 
 })
-saveBtn11am.on('click', function (event) {
-  console.log($(this));
 
-    let userInput = textInput11am.val();
-    var storeEvent = localStorage.setItem('11amEvent', userInput);
-
-})
-saveBtn12am.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput12am.val();
-    var storeEvent = localStorage.setItem('12amEvent', userInput);
-
-})
-saveBtn1pm.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput1pm.val();
-    var storeEvent = localStorage.setItem('1pmEvent', userInput);
-
-})
-saveBtn2pm.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput2pm.val();
-    var storeEvent = localStorage.setItem('2pmEvent', userInput);
-
-})
-saveBtn3pm.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput3pm.val();
-    var storeEvent = localStorage.setItem('3pmEvent', userInput);
-
-})
-saveBtn4pm.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput4pm.val();
-    var storeEvent = localStorage.setItem('4pmEvent', userInput);
-
-})
-saveBtn5pm.on('click', function (event) {
-  console.log($(this));
-
-    let userInput = textInput5pm.val();
-    var storeEvent = localStorage.setItem('5pmEvent', userInput);
-
-})
 
 
 
