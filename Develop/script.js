f74a2dfa231d155fcc6b736b18d21413ac175cb3
Adam Hood <adamhood15@gmail.com>
2023-03-25 12:57:16 -0500

    Final touches to clean up and make comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var get9amEven = localStorage.getItem('9amEvent');
+
+
+//Activates the clock to start on page load
+$(document).ready(function () {
+
 var saveBtn = $('.saveBtn');
-var saveIcon = $('#10amBtn');
 var currentDay = $('#currentDay');
 var event9am = $('#9am')
 var event10am = $('#10am') 
@@ -15,20 +18,10 @@ var event3pm = $('#3pm')
 var event4pm = $('#4pm')
 var event5pm = $('#5pm')
 
-//Activates the clock to start on page load
-$(document).ready(function () {
-  //Displays clock
-  var today = dayjs().format('dddd MMMM DD');
-  currentDay.text(today);
+//Displays clock
+var today = dayjs().format('dddd MMMM DD');
+currentDay.text(today);
 
-}) ;
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 //Displays the localstorage item in the appropriate text box
 event9am.val(localStorage.getItem('hour-9'));
 event10am.val(localStorage.getItem('hour-10'));
@@ -53,45 +46,44 @@ saveBtn.on('click', function () {
 
 })
 
+// stores an array that contains all classes of each element, then grabs the last item in the array
+var hour9Id = parseFloat(($('#hour-9').attr('class').split(' ')).pop());
+var hour10Id = parseFloat(($('#hour-10').attr('class').split(' ')).pop());
+var hour11Id = parseFloat(($('#hour-11').attr('class').split(' ')).pop());
+var hour12Id = parseFloat(($('#hour-12').attr('class').split(' ')).pop());
+var hour13Id = parseFloat(($('#hour-1').attr('class').split(' ')).pop());
+var hour14Id = parseFloat(($('#hour-2').attr('class').split(' ')).pop());
+var hour15Id = parseFloat(($('#hour-3').attr('class').split(' ')).pop());
+var hour16Id = parseFloat(($('#hour-4').attr('class').split(' ')).pop());
+var hour17Id = parseFloat(($('#hour-5').attr('class').split(' ')).pop());
 
-  // TODO: Add code to apply the past, pre
-  // sent, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-// stores an array that contains all classes of each element, then grabs the 3rd item in the array
-// Need to refactor so that it always grabs the last class of the element
-var hour9Id = parseFloat($('#hour-9').attr('class').split(' ')[2]);
-var hour10Id = parseFloat($('#hour-10').attr('class').split(' ')[2]);
-var hour11Id = parseFloat($('#hour-11').attr('class').split(' ')[2]);
-var hour12Id = parseFloat($('#hour-12').attr('class').split(' ')[2]);
-var hour13Id = parseFloat($('#hour-1').attr('class').split(' ')[2]);
-var hour14Id = parseFloat($('#hour-2').attr('class').split(' ')[2]);
-var hour15Id = parseFloat($('#hour-3').attr('class').split(' ')[2]);
-var hour16Id = parseFloat($('#hour-4').attr('class').split(' ')[2]);
-var hour17Id = parseFloat($('#hour-5').attr('class').split(' ')[2]);
+//Stores each hour class in an array as a number
 var hourClassArr = [hour9Id, hour10Id, hour11Id, hour12Id, hour13Id, 
                     hour14Id, hour15Id, hour16Id, hour17Id]
+
+//Stores each element in an array so that we can loop through it
 var hourId = [$('#hour-9'), $('#hour-10'), $('#hour-11'), $('#hour-12'),  
               $('#hour-1'), $('#hour-2'), $('#hour-3'), $('#hour-4'), $('#hour-5'),]
 
-//compares clock to id to apply past, present, future class
-
+//Applies past, present, future class depending on what time it is
+//Must check class of each element and then apply new class to the ID of the element
 for (i = 0; i < hourId.length; i++) {
 
+  //Grabs the hour number from day function
   var hour = dayjs().hour();
 
+  //Applies Present class
   if (hour === hourClassArr[i]) {
 
     hourId[i].addClass('present');
 
+  //Applies future class
   } else if (hour < hourClassArr[i]) {
 
 
     hourId[i].addClass('future');
   
+  //Applies past class
   } else if (hour > hourClassArr[i]) {
 
 
@@ -101,11 +93,5 @@ for (i = 0; i < hourId.length; i++) {
 }
 
 
+}) ;
 
-
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
